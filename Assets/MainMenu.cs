0301d2125786d94f4bf9500003823070abe368c2
Kaya3842 <haichiendhnn1@gmail.com>
2023-06-25 17:05:38 +0700

    Update MainMenu.cs

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Exit the game
     public void ExitButton() {
         Application.Quit();
         Debug.Log("See you next time!");
     }
 
+    // Start the game by going into the fighting scene
     public void StartGame() {
         SceneManager.LoadScene("SampleScene");
     }
