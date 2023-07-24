83d1d2da0d18b98c9fdb1fc1474aa42649f92c52
Kaya3842 <haichiendhnn1@gmail.com>
2023-07-24 10:37:51 +0700

    Audio change

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider musicSlider;  
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("musicVolume")) {
+            LoadVolume();
+        }
+        SetMusicVolume();
+    }
+
+    public void SetMusicVolume() {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void LoadVolume() {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+}
